3d23c60d8a1adcc5594f51043be1be70bfdee421
kt2709 <a.puppala@samsung.com>
2015-03-24 16:13:52 -0700

    click listeners etc added

--- EmPubLite/app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ EmPubLite/app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -13,6 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button=0x7f060000;
+        public static final int checkBox=0x7f060003;
+        public static final int imageView=0x7f060001;
+        public static final int textView=0x7f060002;
+        public static final int toggleButton=0x7f060004;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,11 +2,46 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
-public class EmPubLiteActivity extends Activity {
+public class EmPubLiteActivity extends Activity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+    TextView textView;
+    Button button;
+    CheckBox checkBox;
+    int count=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initapp();
+    }
+
+    private void initapp() {
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        checkBox = (CheckBox)findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        count++;
+        textView.setText(String.format("You clicked %s times",count));
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked)
+            checkBox.setText("You checked");
+        else
+            checkBox.setText("It is unchecked");
     }
 }
